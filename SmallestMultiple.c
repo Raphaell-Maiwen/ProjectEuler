0e111f9c27eff1c4b700ef9a23cff65bf7fcf116
Wafiki789 <raphaelle.mp@gmail.com>
2016-05-02 00:13:54 -0400

    Problem 5 solved! (with other solution)
    Added in comments an interesting solution from a Project Euler user who only used maths.

--- SmallestMultiple.c
+++ SmallestMultiple.c
@@ -3,9 +3,8 @@
 int main(){
 	int multiple = 0;
 	for(int i = 2520; ;i+= 20){
-		//11,12,13,14, 15, 16, 17,18, 19
 		if(i % 11 == 0 && i % 12 == 0 && i % 13 == 0 && i % 14 == 0 && i % 15 == 0 && i % 16 == 0 &&
-		i % 17 == 0 && i % 18 == 0 && i % 19 == 0){	//1,2,4,5,10,20
+		i % 17 == 0 && i % 18 == 0 && i % 19 == 0){
 			multiple = i;
 			break;
 		}
@@ -13,3 +12,23 @@ int main(){
 	printf("Smallest positive number that is divisible by all of the numbers from 1 to 20:\n%d\n", multiple);
 	return 0;
 }
+
+/*
+Answer saw on the forum:
+This does not require programming at all. Compute the prime factorization of each number from 1 to 20, and multiply the greatest power of each prime together:
+
+20 = 2^2 * 5
+19 = 19
+18 = 2 * 3^2
+17 = 17
+16 = 2^4
+15 = 3 * 5
+14 = 2 * 7
+13 = 13
+11 = 11
+
+All others are included in the previous numbers.
+
+ANSWER: 2^4 * 3^2 * 5 * 7 * 11 * 13 * 17 * 19 = 232 792 560
+
+*/
